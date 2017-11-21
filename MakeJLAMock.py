4d208ef0c6db802efebd884f59a7b93992445d48
xyh-cosmo <yhxu@nao.cas.cn>
2017-11-21 14:25:30 +0800

    ...

--- MakeJLAMock.py
+++ MakeJLAMock.py
@@ -16,7 +16,7 @@ var_jla = covmat.diagonal()**0.5
 
 
 # set the random number seed
-np.random.seed(1234567890)
+#np.random.seed(1234567890)
 
 
 def gen_err(covmat=None,use_full_cov=True):
@@ -28,23 +28,24 @@ def gen_err(covmat=None,use_full_cov=True):
 	mean = np.zeros(covmat.shape[0])
 	if use_full_cov is False:
 		covtemp = covmat.copy()
-		covmat.fill(0.)
+		covmat = np.zeros(covtemp.shape)
 		for i in range(ndim):
 			covmat[i,i] = covtemp[i,i]
 	
 	err = np.random.multivariate_normal(mean,covmat)
 	return err
 
-num_of_mocks = 10
+num_of_mocks = 100
+P_VALUE = 0.2
 
 cnt = 0
-while cnt < 10:
+while cnt < num_of_mocks:
 	mu = fun_mu(z_jla)
 	mu_err = gen_err(covmat,use_full_cov=False)
 	ks_stats, pvalue = stats.kstest(mu_err/covmat.diagonal()**0.5,cdf='norm')
-	if pvalue > 0.5:
+	if pvalue >= P_VALUE:
 		print 'yeap! got a good mock sample! pvalue = %g'%(pvalue)
-		fname = 'MOCK_JLA_'+str(cnt+1)+'.txt'
+		fname = 'mock_JLA/MOCK_JLA_'+str(cnt+1)+'.txt'
 		fp = open(fname,'w')
 		for i in range(len(mu)):
 			print >> fp, '%10s %8.6f %10.8f %10.8f %10.8f'%('jla_mock',z_jla[i],mu[i]+mu_err[i],var_jla[i],mu[i])
